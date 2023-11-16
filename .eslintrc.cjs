503e777f1821528c0b50bd059b146a4b9b431165
min <min@ibears.io>
2023-11-16 16:13:01 +0800

    add alias

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,7 +9,20 @@ module.exports = {
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
+  settings: {
+		react: { version: '18.2' },
+		'import/resolver': {
+			node: {
+					extensions: ['.jsx', '.png'],
+			},
+			alias: {
+					extensions: ['.jsx', '.png'],
+					map: [
+							['@/component', './src/component'],
+					],
+			},
+		},
+	},
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
